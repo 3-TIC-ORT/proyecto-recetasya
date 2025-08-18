7be17650f78749ab9b3b0c9beb8fd878bcc48207
Zito3009 <riverzito09@gmail.com>
2025-08-18 11:47:40 -0300

    LOg in hecho

--- RecipEat BACKEND/main.js
+++ RecipEat BACKEND/main.js
@@ -2,9 +2,20 @@ var Usuarios =[
 {
     Nombre_de_la_Cuenta : "Alejo",
     Contraseña : "BOWY123"
-}
-, {
+}, 
+{
     Nombre_de_la_Cuenta : "Lucho",
     Contraseña : "MAYA123"
 }
 ]
+function getInfo() {
+    var Nombre_de_la_Cuenta = document.getElementById('Nombre de la cuenta').value
+    var Contraseña = document.getElementById('Contraseña').value
+    for (var i = 0; i< Usuarios.Lenght; i++) {
+        if (Nombre_de_la_Cuenta == Usuarios[i].Nombre_de_la_Cuenta && Contraseña == Usuarios[i].Contraseña) {
+            alert (Nombre_de_la_Cuenta + " se logeo correctamente")
+            return
+        }
+    }
+    alert ('Contraseña y/onombre de la cuenta incorrectos')
+}
