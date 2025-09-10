a28e56f131d21a9d1ebef3f36e0fbef2551a1e44
bonilauri <bonilauri>
2025-09-10 12:03:50 -0300

    Empece con el segundo boton

--- RecipEat FRONTEND/RecipEat.js
+++ RecipEat FRONTEND/RecipEat.js
@@ -1,4 +1,5 @@
 let boton1 = document.getElementById("boton1");
+let bdulces = document.getElementById("bdulces")
 
 function cambiarpantalla(){
     window.location.href = "pantallasalados.html";
@@ -8,5 +9,10 @@ function cambiarpantalla1(){
     window.location.href = "RecipEat.html";
 }
 
+function cambiardulces(){
+    window.location.href = "pdulces.html";
+}
+
 
 boton1.addEventListener("click", cambiarpantalla);
+bdulces.addEventListener("click", cambiardulces)
