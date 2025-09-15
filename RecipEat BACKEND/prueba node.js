630bd7cd2e35cdd3a09ce14732782e1830363f73
Zito3009 <riverzito09@gmail.com>
2025-09-15 12:06:06 -0300

    log in

--- RecipEat BACKEND/prueba node.js
+++ RecipEat BACKEND/prueba node.js
@@ -1,7 +1,8 @@
 import fs from "fs";
 
 const data = fs.readFileSync("Usurios.json", "utf-8");
-let Usurios = JSON.parse(data);
+let array = JSON.parse(data);
+
 for(let i = 0; i<Usurios.length; i++) {
     console.log(Usurios[i]);
 }
