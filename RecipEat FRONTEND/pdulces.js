62f5ff2a98caf42bb0dfdbb862c3774c5cddf63e
bonilauri <bonilauri>
2025-09-17 12:01:26 -0300

    Termine el boton de dulces

---
+++ RecipEat FRONTEND/pdulces.js
@@ -0,0 +1,13 @@
+let bdulces2 = document.getElementById("bdulces2");
+
+
+function cambiardulces(){
+    window.location.href = "pdulces.html";
+}
+
+function cambiardulces2(){
+    window.location.href = "RecipEat.html";
+}
+
+
+bdulces2.addEventListener("click", cambiardulces2);
