192b2b26d6954cc20fcea20a27605723ab76cb76
Your Name <you@example.com>
2025-09-25 10:40:20 -0300

    Agregue lo nuevo

--- RecipEat FRONTEND/RecipEat.js
+++ RecipEat FRONTEND/RecipEat.js
@@ -1,6 +1,7 @@
 let boton1 = document.getElementById("boton1");
 let bdulces = document.getElementById("bdulces");
 let bbebidas = document.getElementById("bbebidas")
+let bst = document.getElementById("bst")
 
 function cambiarpantalla(){
     window.location.href = "pantallasalados.html";
@@ -18,7 +19,12 @@ function cambiarbebidas(){
     window.location.href = "pbebidas.html"
 }
 
+function cambiarst(){
+    window.location.href = "recetas-st.html"
+}
+
 
 boton1.addEventListener("click", cambiarpantalla);
 bdulces.addEventListener("click", cambiardulces);
 bbebidas.addEventListener("click",cambiarbebidas);
+bst.addEventListener("click", cambiarst);
---
+++ RecipEat FRONTEND/recetas-st.js
@@ -0,0 +1,12 @@
+let bst2 = document.getElementById("bst2")
+
+function cambiarst(){
+    window.location.href = "recetas-st.html"
+}
+
+function cambiarst2(){
+    window.location.href = "RecipEat.html"
+}
+
+
+bst2.addEventListener ("click", cambiarst2);
