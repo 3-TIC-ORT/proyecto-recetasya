ec9b4384c108dd0a3bc9e155a56c78730eded9d2
Zito3009 <riverzito09@gmail.com>
2025-09-29 12:05:38 -0300

    hola

--- RecipEat BACKEND/prueba node.js
+++ RecipEat BACKEND/prueba node.js
@@ -1,8 +1,25 @@
-import fs from "fs";
+import fs, { readFileSync } from "fs";
 
-const data = fs.readFileSync("Usurios.json", "utf-8");
-let array = JSON.parse(data);
+var registro = JSON.parse(fs.readFileSync("Usuarios.json", "utf-8"));
 
-for(let i = 0; i<Usurios.length; i++) {
-    console.log(Usurios[i]);
+var lista = registro
+
+function registrarse () {
+
+for (var i = 0; i<registro.length; i++) {
+    if (gmail === registro[i].GMAIL) {
+        console.log("Este correo electronico ya esta siendo utilizado po otra cuenta");
+        var UsuariosExistentes = true
+    }
+    if (UsuariosExistentes != true) {
+        lista.push ({
+            "NOMBRE": nombre ,
+            "APELLIDO": apellido ,
+            "GMAIL": gmail ,
+            "CONTRASENA": contraseña ,
+        })
+        fs.writeFileSync('Usuarios.json', JSON.stringify(lista));
+        console.log("Usuario registrado con exito");
+    }
+    }
 }
