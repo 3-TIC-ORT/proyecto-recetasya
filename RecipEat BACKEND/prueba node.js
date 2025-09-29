24c0e111453c6177e06a1262bed98da466481604
Zito3009 <riverzito09@gmail.com>
2025-09-29 15:55:20 -0300

    hola

--- RecipEat BACKEND/prueba node.js
+++ RecipEat BACKEND/prueba node.js
@@ -7,7 +7,7 @@ var lista = registro
 function registrarse () {
 
 for (var i = 0; i<registro.length; i++) {
-    if (gmail === registro[i].GMAIL) {
+    if (mail === registro[i].MAIL) {
         console.log("Este correo electronico ya esta siendo utilizado po otra cuenta");
         var UsuariosExistentes = true
     }
@@ -15,11 +15,41 @@ for (var i = 0; i<registro.length; i++) {
         lista.push ({
             "NOMBRE": nombre ,
             "APELLIDO": apellido ,
-            "GMAIL": gmail ,
+            "MAIL": mail ,
             "CONTRASENA": contraseña ,
         })
         fs.writeFileSync('Usuarios.json', JSON.stringify(lista));
         console.log("Usuario registrado con exito");
     }
+    }
+}
+export {registrarse};
+
+function registrarse () {}
+export {registrarse};
+
+function LogueIn () {
+    var nombre = "nombre"
+    var mail = "mail"
+    var contraseña = "contrasena"
+    for (var i = 0; i<registro.length; i++) {
+        if (mail === registro[i].MAIL && contraseña === registro[i].CONTRASENA && nombre === registro[i].NOMBRE) {
+            console.log ("Has iniciado sesion correctamente");
+            var encontrado = true
+            var logeado = true
+            break
+        }
+        else if (contraseña != registro[i].CONTRASENA && mail === registro[i].MAIL) {
+            console.log ("Contraseña incorrecta");
+            var encontrado = true
+            var logeado = false
+            break
+        }
+        else if ((nombre != registro[i].NOMBRE || mail != registro[i].MAIL) && contraseña === registro[i].CONTRASENA) {
+            console.log ("Usuario incorrecto");
+            var encontrado = false
+            var logeado = false
+            break
+        }
     }
 }
