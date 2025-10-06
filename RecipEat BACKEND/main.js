1b31d25aa98e888b32d62bd1cdaaa8429893ad6b
Zito3009 <riverzito09@gmail.com>
2025-10-06 12:04:38 -0300

    HOLA

--- RecipEat BACKEND/main.js
+++ RecipEat BACKEND/main.js
@@ -9,11 +9,11 @@ var Usuarios = [
     }
 ];
 
-function getInfo() {
+function Logearse() {
  
     var correcto = false; 
-    var Nombre_de_la_Cuenta = document.getElementById('Nombre de la cuenta').value
-    var Contraseña = document.getElementById('Contraseña').value
+    var Nombre_de_la_Cuenta = document.getElementById('Nombre de la cuenta').value;
+    var Contraseña = document.getElementById('Contraseña').value;
     for (var i = 0; i< Usuarios.length; i++) {
         if (Nombre_de_la_Cuenta == Usuarios[i].Nombre_de_la_Cuenta && Contraseña == Usuarios[i].Contraseña) {
             correcto = true;
@@ -24,5 +24,23 @@ function getInfo() {
     }
     else if (correcto == false){
         alert("El usuario y la contraseña no coinciden");
+    }
+}
+
+function registrarse() {
+    var correcto = false; 
+    var Nombre_de_la_Cuenta = document.getElementById('Nombre de la cuenta').value;
+    var Contraseña = document.getElementById('Contraseña').value;
+    Usuarios.push (Nombre_de_la_Cuenta && Contraseña);
+    for ( var i = 0; i< Usuarios.lenght; i++) {
+        if (Nombre_de_la_Cuenta == Usuarios[i].Nombre_de_la_Cuenta && Contraseña == Usuarios[i].Contraseña) {
+            correcto = true;
+        }
+    }
+    if (correcto == true){
+        alert("Se guardo correctamente");
+    }
+    else if (correcto == false){
+        alert("No se guardo correctamente");
     }
 }
