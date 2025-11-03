bf5ca4d2af1a35f2ee95b6f14604e7cc7046b981
LucianoBonilauri <luchobonilauri@gmail.com>
2025-11-03 11:59:14 -0300

    recetas puestas. Vamos loos muchachoooos

--- FRONTEND/pantallasalados.js
+++ FRONTEND/pantallasalados.js
@@ -35,6 +35,40 @@ window.onload = () => {
   }
 
 
+
+let comidas = [];
+
+fetch('../BACKEND/recetas.json')          // Ruta al archivo JSON
+  .then(response => response.json())  // Convertir la respuesta en JSON
+  .then(data => {
+    console.log('Comidas cargadas desde JSON:', data);
+    comidas = data;
+    mostrarComidas(comidas);
+  })
+  .catch(error => {
+    console.error('Error al leer el archivo JSON:', error);
+  });
+
+const container = document.getElementById('recetas');
+
+function mostrarComidas(lista) {
+  container.innerHTML = ""; // Limpia antes de mostrar
+
+  lista.forEach(receta => {
+    container.innerHTML += `
+      <div class="tarjeta">
+        <h3>${receta.nombre}</h3>
+        <h4>${receta.categoria}</h4>
+        <h4>${receta.provincia}</h4>
+        <p>${receta.ingredientes || ''}</p>
+      </div>
+    `;
+  });
+}
+
+mostrarComidas
+
+
 function cambiarpantalla(){
     window.location.href = "pantallasalados.html";
 }
