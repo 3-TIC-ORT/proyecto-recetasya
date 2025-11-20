36576b0fdf1015e244029e255928d3e94298d721
Zito3009 <riverzito09@gmail.com>
2025-11-20 01:09:00 -0300

    hola

--- BACKEND/main.js
+++ BACKEND/main.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { loginEvent, registroEvent } from "./LogInRegistro.js";
-import { GuardarRecetas, MostrarRecetas, MostrarRecetasDulces, MostrarRecetasSalados, MostrarRecetasBebidas, RecetasDestacadas, GuardarRecetasFavoritos,  } from "./Recetas.js";
+import { MostrarRecetas, MostrarRecetasDulces, MostrarRecetasSalados, MostrarRecetasBebidas, RecetasDestacadas, GuardarRecetasFavoritos, ObtenerFavoritos, GuardarRecetaCreada, ObtenerRecetarioPersonal } from "./Recetas.js";
 import {
     subscribeGETEvent,
     subscribePOSTEvent,
@@ -16,6 +16,9 @@ subscribeGETEvent("recetasdulces", MostrarRecetasDulces);
 subscribeGETEvent("recetassalados", MostrarRecetasSalados);
 subscribeGETEvent("recetasbebidas", MostrarRecetasBebidas);
 subscribeGETEvent("mostrarRecetasDestacadas", RecetasDestacadas);
+subscribeGETEvent("ObtenerFavoritos", ObtenerFavoritos);
+subscribePOSTEvent("guardarRecetaCreada", GuardarRecetaCreada);
+subscribePOSTEvent("misRecetas", ObtenerRecetarioPersonal);
 
 
 
